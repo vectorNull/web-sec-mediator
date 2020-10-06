4dc1248b09255334ac121c4c31b7eb80198fd3eb
vectorNull <jose.rodriguez76@protonmail.com>
2020-10-06 01:01:08 -0700

    Add registration validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/users
-// @desc    TEST route
-// @access  Public
-router.get('/', (req, res) => res.send('User route'))
+// https://express-validator.github.io/docs/
+const {check, validationResult} = require('express-validator/check')
+
+// route            POST api/users
+// description:     register user
+// access           Public
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        res.send('User route')
+});
 
 module.exports = router;
