899171d2ef2fab999f21d9664bf30fe3b7e952a1
vectorNull <jose.rodriguez76@protonmail.com>
2020-10-10 15:18:01 -0700

    Create Alert component

---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Alert = ({ alerts }) => 
+    alerts !== null && alerts.length > 0 && alerts.map(alert => (
+        <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+            { alert.msg }
+        </div>
+    ));
+
+Alert.propTypes = {
+    alerts: propTypes.array.isRequired
+}
+
+const mapStateToProp = state => ({
+    alerts: state.alert
+})
+
+export default connect(mapStateToProp)(Alert);
