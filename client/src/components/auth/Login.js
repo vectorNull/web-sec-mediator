433780ec8515ecfddd73c1edd95798e03f951998
vectorNull <jose.rodriguez76@protonmail.com>
2020-10-10 21:07:46 -0700

    Redirect to dashboard

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { login } from '../../actions/auth'
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         email: '',
         password: '',
@@ -13,9 +16,14 @@ const Login = () => {
     
     const onSubmit = async e => {
         e.preventDefault();       
-        console.log('Success!!')
+        login(email, password);
         }
-    
+	
+	// Redirect if logged in
+	if(isAuthenticated) {
+		return <Redirect to="/dashboard" />
+	}	
+
     return (
 		<Fragment>
 			<h1 className='large text-primary'>Sign In</h1>
@@ -53,4 +61,13 @@ const Login = () => {
 	);
 }
 
-export default Login;
+Login.propTypes = {
+	login: propTypes.func.isRequired,
+	isAuthenticated: propTypes.bool,
+};
+
+const mapStateToProps = state => ({
+	isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
