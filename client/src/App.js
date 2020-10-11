4c00317220a86e8dc3815c571a98a184417a65ae
vectorNull <jose.rodriguez76@protonmail.com>
2020-10-10 21:06:37 -0700

    Set local storage token

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import { loadUser } from './actions/auth';
 import './App.css';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
 
-const App = () => 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => { 
+  
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -27,5 +39,5 @@ const App = () =>
       </Fragment>
     </Router> 
   </Provider>
-
+)};
 export default App;
