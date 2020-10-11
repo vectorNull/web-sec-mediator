4eb09118d061c3c3948fbe179b4b15cb7755217d
vectorNull <jose.rodriguez76@protonmail.com>
2020-10-10 21:08:35 -0700

    Refactor reducer for login

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,5 +1,5 @@
 // reducer ==> actions/auth.js
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS } from '../actions/types';
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -12,10 +12,15 @@ export default function(state = initialState, action) {
     const { type, payload } = action;
     
     switch(type) {
+        case USER_LOADED:
+            return { ...state, isAuthenticated: true, loading: false, user: payload }
         case REGISTER_SUCCESS:
-            localStorage.setItem('token', payload.token);
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token', payload.token);  // places into local storage
             return { ...state, ...payload, isAuthenticated: true, loading: false}
         case REGISTER_FAIL:
+        case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem('token');
             return { ...state, token: null, isAuthenticated: false, loading: false}        
         default:
