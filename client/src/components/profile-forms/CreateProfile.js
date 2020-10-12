db02699d885463f6bfd521508538beb668369e37
vectorNull <jose.rodriguez76@protonmail.com>
2020-10-12 16:45:21 -0700

    Refactor

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -41,7 +41,7 @@ const CreateProfile = ({ createProfile, history }) => {
 	const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
 	const onSubmit = e => {
 		e.preventDefault();
-		createProfile(formData, history, true);
+		createProfile(formData, history);
 	};
 
 	return (
@@ -178,9 +178,9 @@ const CreateProfile = ({ createProfile, history }) => {
 				)}
 
 				<input type='submit' className='btn btn-primary my-1' />
-				<a className='btn btn-light my-1' href='dashboard.html'>
+				<Link className='btn btn-light my-1' to='/dashboard'>
 					Go Back
-				</a>
+				</Link>
 			</form>
 		</Fragment>
 	);
