2da1e5f05fb8af0c7810a4b8700318e1d7b14cc6
vectorNull <jose.rodriguez76@protonmail.com>
2020-10-13 16:58:55 -0700

    Experience and education routes

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,9 +4,11 @@ import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/spinner';
 import DashBoardActions from './DashboardActions';
-import { getCurrentProfile } from '../../actions/profiles';
+import Experience from './Experience';
+import Education from './Education';
+import { deleteAccount, getCurrentProfile } from '../../actions/profiles';
 
-const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading }}) => {
+const Dashboard = ({ getCurrentProfile, deleteAccount, auth: { user }, profile: { profile, loading }}) => {
     
     useEffect(() => {
         getCurrentProfile();
@@ -21,6 +23,14 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
         { profile !== null ? ( 
         <Fragment>
             <DashBoardActions />
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
+
+            <div className="my-2">
+                <button className="btn btn-danger" onClick={() => deleteAccount()}>
+                    <i className="fas fas-user-minus"></i> Delete My Account
+                </button>   
+            </div>
         </Fragment> 
         ) : (
         <Fragment>
@@ -32,6 +42,7 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
 
 Dashboard.propTypes = {
     getCurrentProfile: propTypes.func.isRequired,
+    deleteAccount: propTypes.func.isRequired,
     auth: propTypes.object.isRequired,
     profile: propTypes.object.isRequired,
 }
@@ -41,4 +52,4 @@ const mapStateToProps = state => ({
     profile: state.profile
 })
 
-export default connect(mapStateToProps, { getCurrentProfile}) (Dashboard)
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount }) (Dashboard)
