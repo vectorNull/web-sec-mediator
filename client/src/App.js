d54b5aeb22225b4c0180da6aa331718856529131
vectorNull <jose.rodriguez76@protonmail.com>
2020-10-13 18:50:22 -0700

    Import Profiles and route

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth';
 import PrivateRoute from './components/routing/PrivateRoute';
@@ -40,6 +41,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
