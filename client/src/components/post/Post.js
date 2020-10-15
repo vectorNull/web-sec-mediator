0b3e143608a313c7f4aa8b1352242faec762c7c8
vectorNull <jose.rodriguez76@protonmail.com>
2020-10-14 17:18:46 -0700

    Refactor to display comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/spinner';
 import PostItem from '../posts/PostItem';
-import CommentForm from '../post/CommentForm'
+import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -16,9 +17,20 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
 		<Spinner />
 	) : (
 		<Fragment>
-            <Link to='/posts' className='btn'>Back To Posts</Link>
+			<Link to='/posts' className='btn'>
+				Back To Posts
+			</Link>
 			<PostItem post={post} showActions={false} />
-            <CommentForm postId={post._id} />
+			<CommentForm postId={post._id} />
+			<div className='comments'>
+				{post.comments.map((comment) => (
+					<CommentItem
+						key={comment._id}
+						comment={comment}
+						postId={post._id}
+					/>
+				))}
+			</div>
 		</Fragment>
 	);
 };
